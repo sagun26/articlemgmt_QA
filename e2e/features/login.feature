5738a150831c467a8498085294b375d5128db649
Sagun Sigdel <sagunsigdel6@gmail.com>
2026-08-17 14:22:20 +0545

    initial commit

---
+++ e2e/features/login.feature
@@ -0,0 +1,12 @@
+Feature: login
+As a user
+I want to login to the website 
+so that i can access the website 
+
+Scenario: login with valid credentials
+Given I am on the login page
+When I enter valid username and password
+|Name|
+|muna|
+
+Then the user logged in successfully and redirected to the home page
---
+++ e2e/stepDefinations/loginContext.ts
@@ -0,0 +1,14 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+import { expect } from "@playwright/test";
+
+Given("I am on the login page", async function () {
+  // Add step action
+});
+
+When("I enter valid username and password", async function (dataTable) {
+  // Add step action
+});
+
+Then("the user logged in successfully and redirected to the home page", async function () {
+  // Add step action
+});
