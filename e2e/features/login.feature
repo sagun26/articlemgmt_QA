78350112cff3040c9defae8fba6b0d29015fd2e4
Sagun Sigdel <sagunsigdel6@gmail.com>
2026-08-17 17:47:47 +0545

    latest

--- e2e/features/login.feature
+++ e2e/features/login.feature
@@ -5,8 +5,9 @@ so that i can access the website
 
 Scenario: login with valid credentials
 Given I am on the login page
-When I enter valid username and password
-|Name|
-|muna|
+When I enter valid username
+|Name |
+|muna |
+|samir|
 
 Then the user logged in successfully and redirected to the home page
--- e2e/stepDefinitions/loginContext.ts
+++ e2e/stepDefinitions/loginContext.ts
@@ -3,12 +3,16 @@ import { expect } from "@playwright/test";
 
 Given("I am on the login page", async function () {
   // Add step action
+
+  console.log("I am on the login page");
 });
 
-When("I enter valid username and password", async function (dataTable) {
+When("I enter valid username", async function (dataTable) {
   // Add step action
+  console.log("I am entering valid username page");
 });
 
 Then("the user logged in successfully and redirected to the home page", async function () {
   // Add step action
+  console.log("User logged in successfully and redirected to the home page");
 });
